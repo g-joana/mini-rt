7428080c81aa3a2c44e88f308cd088bf5a15d92f
jou <jou.42.rio@gmail.com>
2025-07-17 10:45:55 -0300

    check point

--- src/render/get_shape_hit.c
+++ src/render/get_shape_hit.c
@@ -1,4 +1,5 @@
 #include "../../includes/minirt.h"
+#include <stdio.h>
 
 t_hit *sphere_hit(t_ray *ray, t_sphere *sp)
 {
@@ -23,28 +24,31 @@ t_hit *sphere_hit(t_ray *ray, t_sphere *sp)
 	return hit;
 }
 
-t_vec3d rotate_to_z_axis(t_vec3d *v, t_vec3d *norm) {
+t_vec3d transform_to_z_axis(t_vec3d *v, t_vec3d *norm, int clock) {
 
     t_vec3d z_axis = {0, 0, 1};
     *norm = norm_vec(norm);
     float cos_theta = dot_vecs(norm, &z_axis);
-    if (cos_theta > 0.9999f) // already aligned
+    if (cos_theta > 0.0001f) // already aligned
         return *v;
-    if (cos_theta < -0.9999f) // opposite direction
+    if (cos_theta < -0.0001f) // opposite direction
         return (t_vec3d){-v->x, -v->y, -v->z};
 
     float theta = acosf(cos_theta);
-    t_vec3d axis = cross_vecs(norm, &z_axis);
+    t_vec3d axis;
+    axis = cross_vecs(norm, &z_axis);
     axis = norm_vec(&axis);
 
+    if (clock == -1)
+        theta = -theta;
     float c = cosf(theta);
     float s = sinf(theta);
 
-    t_vec3d v_rot;
-    v_rot.x = v->x * c + axis.x * dot_vecs(&axis, v) * (1 - c) + (-axis.z * v->y + axis.y * v->z) * s;
-    v_rot.y = v->y * c + axis.y * dot_vecs(&axis, v) * (1 - c) + ( axis.z * v->x - axis.x * v->z) * s;
-    v_rot.z = v->z * c + axis.z * dot_vecs(&axis, v) * (1 - c) + (-axis.y * v->x + axis.x * v->y) * s;
-    return v_rot;
+    t_vec3d transform;
+    transform.x = v->x * c + axis.x * dot_vecs(&axis, v) * (1 - c) + (-axis.z * v->y + axis.y * v->z) * s;
+    transform.y = v->y * c + axis.y * dot_vecs(&axis, v) * (1 - c) + ( axis.z * v->x - axis.x * v->z) * s;
+    transform.z = v->z * c + axis.z * dot_vecs(&axis, v) * (1 - c) + (-axis.y * v->x + axis.x * v->y) * s;
+    return transform;
 }
 
 t_hit *cylinder_hit(t_ray *ray, t_cylinder *cy)
@@ -54,9 +58,10 @@ t_hit *cylinder_hit(t_ray *ray, t_cylinder *cy)
 	t_vec3d dir;
 	t_vec3d ori; 
 
-	dir = rotate_to_z_axis(&ray->dir, cy->norm);
+	// *cy->norm = norm_vec(cy->norm);
+	dir = transform_to_z_axis(&ray->dir, cy->norm, 1);
 	dir.z = 0;
-	ori = rotate_to_z_axis(&ray->ori, cy->norm);
+	ori = transform_to_z_axis(&ray->ori, cy->norm, 1);
 	ori.z = 0;
 
 	float r = cy->diam/2;
@@ -73,12 +78,10 @@ t_hit *cylinder_hit(t_ray *ray, t_cylinder *cy)
 
 	hit = malloc(sizeof(t_hit));
 	hit->distance = t;
+    // local position
 	hit->position = vec_x_scalar(&ray->dir, hit->distance);
 	hit->position = add_vecs(&ray->ori, &hit->position);
 
-	// define limits
-	t_vec3d half_height_ratio = vec_x_scalar(cy->norm, cy->height / 2.0f);
-	t_vec3d base = sub_vecs(cy->coord, &half_height_ratio);
 	t_vec3d center = sub_vecs(&hit->position, cy->coord);
 	float len = dot_vecs(&center, cy->norm);
 	if (len < -cy->height/2.0f || len > cy->height /2.0f)
@@ -86,10 +89,16 @@ t_hit *cylinder_hit(t_ray *ray, t_cylinder *cy)
 		free(hit);
 		return NULL;
 	}
-	hit->shape = CY;
 	t_vec3d axis = vec_x_scalar(cy->norm, len);
+    // local normal
 	hit->direction = sub_vecs(&center, &axis);
+    hit->direction = cross_vecs(&hit->position, cy->norm);
 	hit->direction = norm_vec(&hit->direction);
+    // world normal
+    hit->position = transform_to_z_axis(&hit->position, cy->norm, -1);
+    hit->direction = transform_to_z_axis(&hit->direction, cy->norm, -1);
+	hit->direction = norm_vec(&hit->direction);
+	hit->shape = CY;
 	return hit;
 }
 
