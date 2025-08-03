d80119a0ed2b180ef27b2328f0a2f2dd05c98f44
jou <jou.42.rio@gmail.com>
2025-08-03 18:58:39 -0300

    cy get shape checkpoint

--- src/render/get_shape_hit.c
+++ src/render/get_shape_hit.c
@@ -52,58 +52,76 @@ t_hit *sphere_hit(t_ray *ray, t_sphere *sp)
 	return hit;
 }
 
+float cy_quadratic(t_ray *ray, float radius, float *t)
+{
+	float a = dot_vecs(&ray->dir, &ray->dir);
+	float b = 2.0f * dot_vecs(&ray->ori, &ray->dir);
+	float c = dot_vecs(&ray->ori, &ray->ori) - radius * radius;
+
+    float delta = b * b - 4.0f * a * c;
+	if (delta < 0.0f)
+	    return 0;
+	t[1] = (-b - sqrtf(delta)) / (2.0f * a);
+    t[2] = (-b + sqrtf(delta)) / (2.0f * a);
+    t[0] = t[1];
+	if (t[0] < 0.001f)
+	    t[0] = t[2];
+	return t[0];
+}
+
+t_ray get_cy_ray(t_ray *ray, t_cylinder *cy, float *proj)
+{
+    t_ray axis;
+    t_ray local;
+
+    axis.ori = vec_x_scalar(cy->norm, proj[0]);
+    axis.dir = vec_x_scalar(cy->norm, proj[1]);
+    local.ori = sub_vecs(&ray->ori, &axis.ori);
+    local.dir = sub_vecs(&ray->dir, &axis.dir);
+    return local;
+}
+
+int valid_t(float *proj, float t, float cy_height)
+{
+    float hit_axis_pos;
+
+    hit_axis_pos = proj[0] + t * proj[1];
+    if (hit_axis_pos < -cy_height/2.0f || hit_axis_pos > cy_height/2.0f)
+        return 0;
+    return 1;
+}
+
 t_hit *cylinder_hit(t_ray *ray, t_cylinder *cy)
 {
     t_hit *hit;
+    float hit_axis_pos;
+    float proj[2];
+    t_ray local;
+    float t[3];
     
-    float proj_origin = dot_vecs(&ray->ori, cy->norm);
-    float proj_dir = dot_vecs(&ray->dir, cy->norm);
-    
-    t_vec3d axis_ori = vec_x_scalar(cy->norm, proj_origin);
-    t_vec3d axis_dir = vec_x_scalar(cy->norm, proj_dir);
-    
-    t_vec3d ori = sub_vecs(&ray->ori, &axis_ori);
-    t_vec3d dir = sub_vecs(&ray->dir, &axis_dir);
-    
-    float r = cy->diam / 2.0f;
-    float a = dot_vecs(&dir, &dir);
-    float b = 2.0f * dot_vecs(&ori, &dir);
-    float c = dot_vecs(&ori, &ori) - r * r;
-    
-    float delta = b * b - 4.0f * a * c;
-    if (delta < 0.0f)
+    proj[0] = dot_vecs(&ray->ori, cy->norm);
+    proj[1] = dot_vecs(&ray->dir, cy->norm);
+    local = get_cy_ray(ray, cy, proj);
+    t[0] = cy_quadratic(&local, cy->diam/2, t);
+    if (t[0] < 0.001f)
         return NULL;
-    float t1 = (-b - sqrtf(delta)) / (2.0f * a);
-    float t2 = (-b + sqrtf(delta)) / (2.0f * a);
-
-    float t = t1;
-    if (t < 0.001f)
-	t = t2;
-    if (t < 0.001f)
-	return NULL;
-
-
-    float hit_axis_pos = proj_origin + t * proj_dir;
+    hit_axis_pos = proj[0] + t[0] * proj[1];
     if (hit_axis_pos < -cy->height/2.0f || hit_axis_pos > cy->height/2.0f)
     {
-	if (t == t1)
-	    t = t2;
-	if (t < 0.001f)
-	    return NULL;
-
-	hit_axis_pos = proj_origin + t * proj_dir;
-	if (hit_axis_pos < -cy->height/2.0f || hit_axis_pos > cy->height/2.0f)
-	    return NULL;
+        if (t[0] == t[1] && t[2] > 0.001f)
+            t[0] = t[2];
+        else
+            return NULL;
+        if (!valid_t(proj, t[2], cy->height))
+            return NULL;
     }
-    
     hit = malloc(sizeof(t_hit));
     if (!hit)
         return NULL;
-    hit->distance = t;
-    if (t == t2)
-	hit->inside = 1;
-    else
-	hit->inside = 0;
+    hit->distance = t[0];
+    hit->inside = 0;
+    if (t[0] == t[2])
+        hit->inside = 1;
     return hit;
 }
 
